23d99364ef556479be21e9a4beed55b6de88f84e
Palak <palaktiwari156@gmail.com>
2021-11-19 23:40:56 +0530

    create-react-app

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="App">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit porro, harum deleniti quasi labore sequi eveniet perspiciatis repudiandae dicta optio eius ex dignissimos quia ad aperiam corporis quas, debitis ducimus expedita totam voluptate. Consequuntur veritatis voluptate, possimus mollitia numquam facilis labore eos deleniti cupiditate expedita nesciunt. Ut autem ex corporis placeat! Dolor soluta eos facere temporibus corporis nisi corrupti at!
+
+    <h4>I'm heading 4</h4>
+
+    <h3>I'm heading 3</h3>
+
+    
+    </div>
+  );
+}
+
+export default App;
