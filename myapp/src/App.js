3fbc312c1eda7a10541fff799ee8d8cb0d1a4c9d
Palak <palaktiwari156@gmail.com>
2021-11-25 12:46:35 +0530

    react component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,8 @@
-
-function App() {
+import Navbar from './Navbar'
+const App = () =>  {
   return (
     <div className="App">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit porro, harum deleniti quasi labore sequi eveniet perspiciatis repudiandae dicta optio eius ex dignissimos quia ad aperiam corporis quas, debitis ducimus expedita totam voluptate. Consequuntur veritatis voluptate, possimus mollitia numquam facilis labore eos deleniti cupiditate expedita nesciunt. Ut autem ex corporis placeat! Dolor soluta eos facere temporibus corporis nisi corrupti at!
-
-    <h4>I'm heading 4</h4>
-
-    <h3>I'm heading 3</h3>
-
-    
+      <Navbar />
     </div>
   );
 }
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <div className="class"></div>
   </React.StrictMode>,
   document.getElementById('root')
 );
