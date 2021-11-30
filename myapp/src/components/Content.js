544b730c944d37c5849f8efc1f837b5bc62faa71
Palak <palaktiwari156@gmail.com>
2021-11-30 17:33:50 +0530

    todo - using usestate - 1

--- myapp/src/components/Content.js
+++ myapp/src/components/Content.js
@@ -3,22 +3,42 @@ import {useState} from 'react'
 
 const Content = () => {
 
-    const [counter, setCounter] = useState(0)
-    // console.log(counter)
-    // console.log(mysteryfunction)
+    const [items, setItems] = useState([])
+    const [value, setValue] = useState('')
 
-    return (
-        <div className="container">
-            <h2>{counter}</h2>
-            <button onClick={() => {
-                // counter = counter + 1
-                // console.log(counter)
-                setCounter(previousValue => {
-                    console.log(previousValue)
-                    return previousValue+1
-                })
-            }}>Click Me!</button>
-        </div>
+    const handleSubmit = () => {
+        
+        console.log(value)
+
+        setItems(prev => [...prev, value])
+        setValue('')
+    }
+
+
+    const handleInput = (e) => {
+        setValue(e.target.value)
+    }
+
+    //console.log(value)
+
+    const handleDelete = (item) =>{
+        console.log(item)
+        setItems(prev => prev.filter(i => i !== item))
+    }
+    
+    return ( 
+        <>
+            <h2>Todo List</h2>
+            
+                <input value={value} onChange={handleInput} type="text" />
+                <button onClick={handleSubmit} type="submit">Submit</button>
+            
+            
+            <ul>
+                {items.map(item => <li>{item} <button onClick={() => {handleDelete(item) }}>Delete</button></li>)}
+            </ul>
+        </>
+
     );
 }
 
