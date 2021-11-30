59c2db8fa4372a1a14bcfbb488ea75c663b8fa83
Palak <palaktiwari156@gmail.com>
2021-11-30 21:41:59 +0530

    useeffect hook

--- myapp/src/components/Content.js
+++ myapp/src/components/Content.js
@@ -1,42 +1,21 @@
 //import ListItem from './ListItem'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 const Content = () => {
+    
+    //console.log('render')
+    const [counter, setCounter] = useState(0)
 
-    const [items, setItems] = useState([])
-    const [value, setValue] = useState('')
-
-    const handleSubmit = () => {
-        
-        console.log(value)
-
-        setItems(prev => [...prev, value])
-        setValue('')
-    }
-
-
-    const handleInput = (e) => {
-        setValue(e.target.value)
-    }
-
-    //console.log(value)
-
-    const handleDelete = (item) =>{
-        console.log(item)
-        setItems(prev => prev.filter(i => i !== item))
-    }
+    useEffect(() => {
+        console.log('Mounted')
+    }, [])
     
     return ( 
         <>
-            <h2>Todo List</h2>
-            
-                <input value={value} onChange={handleInput} type="text" />
-                <button onClick={handleSubmit} type="submit">Submit</button>
-            
-            
-            <ul>
-                {items.map(item => <li>{item} <button onClick={() => {handleDelete(item) }}>Delete</button></li>)}
-            </ul>
+            <h2>{counter}</h2>
+            <button onClick= {() => { 
+                setCounter(prev => prev + 1) }}>Click Me!</button>
+           
         </>
 
     );
