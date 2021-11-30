7df215c05635247872fb4c99290b0f84c0cb443d
Palak <palaktiwari156@gmail.com>
2021-11-30 21:54:23 +0530

    api call on mounting

--- myapp/src/components/Content.js
+++ myapp/src/components/Content.js
@@ -5,16 +5,26 @@ const Content = () => {
     
     //console.log('render')
     const [counter, setCounter] = useState(0)
+    const [content, setContent] = useState([])
+
+    const APICall = async () => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+        const data = await res.json()
+        setContent(data)
+    }
 
     useEffect(() => {
-        console.log('Mounted')
+        APICall()
     }, [])
     
     return ( 
         <>
             <h2>{counter}</h2>
-            <button onClick= {() => { 
-                setCounter(prev => prev + 1) }}>Click Me!</button>
+            <button onClick= {() => { setCounter(prev => prev + 1) }}>Click Me!</button>
+
+            <ul>
+                {content.map(item => <li>{item.title}</li>)}
+            </ul>
            
         </>
 
