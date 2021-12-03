8c321307cff9141e1b0d0320aa5354e386f81b0e
Palak <palaktiwari156@gmail.com>
2021-12-03 22:09:28 +0530

    higher order components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,12 @@
-import {useState} from 'react'
-import ChildOne from "./components/childOne";
+import Description from "./components/Description";
+
 
 const App = () =>  {
 
-  const [data, setData] = useState([{name: "Apple"}, {name: "Samsung"}, {name: "Nokia"}])
-
+  
   return (
     <div className="App">
-      <h1 style={{color: "red"}}>I'm the Parent</h1>
-      <ChildOne data={data} />
+      <Description heading={'Mercedes S-Class'} />
     </div>
   );
 
